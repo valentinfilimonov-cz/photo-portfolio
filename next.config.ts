fdde5f5f65db4b7964c2ad351415866a9d2faa18
Valentin F <valentin.filimonov@gmail.com>
2026-06-19 11:59:43 +0200

    Fix Decap CMS setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
